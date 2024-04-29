1fd72b7d2a7b714ca73b1118b6cb814f5c9eb4b8
Travis Luong <travis@travisluong.com>
2024-04-29 16:54:47 +0800

    etl load csv to mysql

--- lib/schema.ts
+++ lib/schema.ts
@@ -9,10 +9,13 @@ import {
 export const problems = mysqlTable("problems", {
   id: serial("id").primaryKey(),
   name: varchar("name", { length: 256 }),
+  slug: varchar("slug", { length: 256 }),
   category: varchar("category", { length: 256 }),
   difficulty: mysqlEnum("difficulty", ["easy", "medium", "hard"]),
   leetcode_url: varchar("leetcode_url", { length: 256 }),
   youtube_url: varchar("youtube_url", { length: 256 }),
   github_url: varchar("github_url", { length: 256 }),
+  neetcode_url: varchar("neetcode_url", { length: 256 }),
   notes: text("notes"),
+  solution: text("solution"),
 });
---
+++ scripts/etl-csv-to-mysql.ts
@@ -0,0 +1,63 @@
+import mysql from "mysql2/promise";
+import fs from "fs";
+import csv from "csv-parser";
+import dotenv from "dotenv";
+
+dotenv.config({ path: ".env.local" });
+
+// MySQL database connection settings
+const dbConfig: mysql.PoolOptions = {
+  uri: process.env.DB_URL!,
+};
+
+// Create a MySQL connection pool
+const pool = mysql.createPool(dbConfig);
+
+// Function to load data from CSV file into MySQL database
+async function loadDataFromCSV() {
+  const csvFilePath = process.argv[2]; // Path to your CSV file
+
+  fs.createReadStream(csvFilePath)
+    .pipe(csv())
+    .on("data", async (row) => {
+      // Map CSV columns to MySQL table columns
+      const leetcodeUrl = row["Link"];
+      const pathSegments = leetcodeUrl
+        .split("/")
+        .filter((segment: string) => segment !== ""); // Split path and remove empty segments
+      const lastSubpath = pathSegments[pathSegments.length - 1]; // Get the last subpath
+
+      const data = {
+        neetcode_url: row["Video Solution"],
+        category: row["Category"],
+        name: row["Name"],
+        leetcode_url: row["Link"],
+        notes: row["Notes"],
+        slug: lastSubpath,
+      };
+
+      console.log(data);
+
+      try {
+        // Insert data into MySQL table
+        const [results, fields] = await pool.query(
+          "INSERT INTO problems SET ?",
+          data
+        );
+        console.log("Data inserted successfully");
+      } catch (error) {
+        console.error("Error inserting data:", error);
+      }
+    })
+    .on("end", () => {
+      console.log("CSV file successfully processed");
+      // Close MySQL connection pool
+      // pool.end();
+    })
+    .on("error", (error) => {
+      console.error("Error reading CSV file:", error);
+    });
+}
+
+// Load data from CSV into MySQL database
+loadDataFromCSV();
