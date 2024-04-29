3e351a7bf248b4045753d4e4efe2f203068c58cf
Travis Luong <travis@travisluong.com>
2024-04-29 12:55:26 +0800

    deetcode lib proof of concept

---
+++ app/contains-duplicate/page.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { Editor } from "@monaco-editor/react";
+import { useEffect, useRef } from "react";
+import dc from "@/lib/deetcode";
+
+/**
+ * Hash Set - Early Exit
+ * Time O(N) | Space O(N)
+ * https://leetcode.com/problems/contains-duplicate/
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+
+var code = `
+var containsDuplicate = (nums) => {
+  const numSet = new Set();
+  debugger;
+  for (const num of nums) {
+    if (numSet.has(num)) return true;
+    numSet.add(num);
+  }
+  return false;
+};
+
+const arr = [1, 2, 3, 1];
+const res = containsDuplicate(arr);
+console.log(res);
+`;
+
+export default function Page() {
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      dc.configure({ selector: "#deetcode" });
+      window.dc = dc;
+    }
+  }, []);
+
+  const editorRef = useRef(null);
+
+  function handleEditorDidMount(editor: any, monaco: any) {
+    editorRef.current = editor;
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const code = editorRef.current.getValue();
+    console.log(code);
+    dc.monkeyPatch(window);
+    eval(code);
+    dc.undoMonkeyPatch(window);
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <Editor
+          height="50vh"
+          defaultLanguage="javascript"
+          defaultValue={code}
+          onMount={handleEditorDidMount}
+        />
+        <button>submit</button>
+      </form>
+      <div id="deetcode"></div>
+    </div>
+  );
+}
---
+++ lib/deetcode.ts
@@ -0,0 +1,69 @@
+"use client";
+
+interface DeetCode {
+  DeetSet: typeof DeetSet;
+  configure: (config: { selector: string }) => void;
+  element?: Element | null;
+  monkeyPatch: (window: any) => void;
+  originalAdd?: (value: any) => Set<any>;
+  undoMonkeyPatch: (window: any) => void;
+}
+
+export class DeetSet {
+  set: Set<any>;
+
+  constructor(objs?: any[]) {
+    this.set = new Set(objs);
+  }
+
+  has(obj: any) {
+    console.log("adding to deetset");
+
+    return this.set.has(obj);
+  }
+
+  add(obj: any) {
+    const div = document.createElement("div");
+    div.innerHTML = obj;
+    dc.element?.appendChild(div);
+    return this.set.add(obj);
+  }
+}
+
+const dc: DeetCode = {
+  DeetSet: DeetSet,
+  configure,
+  monkeyPatch,
+  undoMonkeyPatch,
+};
+
+function configure(config: { selector: string }) {
+  dc.element = document.querySelector(config.selector);
+}
+
+function monkeyPatch(window) {
+  debugger;
+  if (dc.originalAdd === undefined) {
+    dc.originalAdd = Set.prototype.add;
+  }
+
+  Set.prototype.add = function (value) {
+    // Call the original add method to ensure normal functionality
+    dc.originalAdd?.call(this, value);
+    if (window.dc.element) {
+      const div = document.createElement("div");
+      div.innerHTML = value;
+      dc.element?.appendChild(div);
+    }
+
+    return this;
+  };
+}
+
+function undoMonkeyPatch(window) {
+  if (dc.originalAdd) {
+    Set.prototype.add = dc.originalAdd;
+  }
+}
+
+export default dc;
