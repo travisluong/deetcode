aafead06d567caff581557bcc97bb162f37c553d
Travis Luong <travis@travisluong.com>
2024-06-02 18:28:07 +0800

    update migrate script to accept a prod arg

--- scripts/migrate.ts
+++ scripts/migrate.ts
@@ -1,9 +1,18 @@
-import { config } from "@/lib/config";
 import { drizzle } from "drizzle-orm/mysql2";
 import { migrate } from "drizzle-orm/mysql2/migrator";
 import { createConnection } from "mysql2";
+import dotenv from "dotenv";
 
-const connection = createConnection(config.DB_URL);
+dotenv.config({ path: ".env.local" });
+
+const url =
+  process.argv[2] === "prod" ? process.env.PROD_MYSQL_URL : process.env.DB_URL;
+
+if (!url) {
+  throw new Error("url not found");
+}
+
+const connection = createConnection(url);
 const db = drizzle(connection);
 await migrate(db, { migrationsFolder: "drizzle" });
 await connection.end();
