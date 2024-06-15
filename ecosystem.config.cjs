1a884b561de119936cb106e9eb24d138e793ad06
Travis Luong <travis@travisluong.com>
2024-06-15 21:03:55 +0800

    add veetcode to ecosystem config

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -7,6 +7,9 @@ module.exports = {
       env_production: {
         PORT: 8005,
       },
+      env_veetcode: {
+        PORT: 8007,
+      },
     },
   ],
 
@@ -20,6 +23,18 @@ module.exports = {
       "pre-deploy-local": "",
       "post-deploy":
         "source ~/.nvm/nvm.sh && nvm use 20 && npm install && npm run build && pm2 reload ecosystem.config.cjs --env production",
+      "pre-setup": "",
+      ssh_options: "ForwardAgent=yes",
+    },
+    veetcode: {
+      user: "veetcode",
+      host: "veetcode.com",
+      ref: "origin/main",
+      repo: "git@github.com:travisluong/deetcode.git",
+      path: "/home/veetcode/veetcode.com",
+      "pre-deploy-local": "",
+      "post-deploy":
+        "source ~/.nvm/nvm.sh && nvm use 20 && npm install && npm run build && pm2 reload ecosystem.config.cjs --env veetcode",
       "pre-setup": "",
       ssh_options: "ForwardAgent=yes",
     },
