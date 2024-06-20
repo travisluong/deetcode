524c84d9642f372a85b0b67a00837ebabf419b29
Travis Luong <travis@travisluong.com>
2024-06-20 13:41:40 +0800

    update generate problem list script

--- scripts/generate-blind-75-list.ts
+++ scripts/generate-blind-75-list.ts
@@ -1,5 +1,6 @@
 import { problem_to_list } from "@/lib/schema";
 import { connection, db } from "./script-db";
+import { sql } from "drizzle-orm";
 
 const problems = await db.query.problems.findMany();
 const problemList = await db.query.problem_list.findFirst();
@@ -11,7 +12,8 @@ if (!problemList) {
 for (const problem of problems) {
   await db
     .insert(problem_to_list)
-    .values({ problem_id: problem.id, list_id: problemList.id });
+    .values({ problem_id: problem.id, list_id: problemList.id })
+    .onDuplicateKeyUpdate({ set: { problem_id: sql`problem_id` } });
 }
 
 connection.end();
