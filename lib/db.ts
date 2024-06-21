b75e38ef51f34dbdc9d4889a79f5849f395c536d
Travis Luong <travis@travisluong.com>
2024-06-21 20:15:44 +0800

    put mysql connection in global var to prevent too many connections caused by hot module reloading

--- lib/db.ts
+++ lib/db.ts
@@ -1,10 +1,26 @@
 import { drizzle } from "drizzle-orm/mysql2";
-import mysql from "mysql2/promise";
+import mysql, { Pool } from "mysql2/promise";
 import { config } from "./config";
 import * as schema from "./schema";
 
-const poolConnection = mysql.createPool({
-  uri: config.DB_URL,
-});
+declare global {
+  var poolConnection: Pool;
+}
+
+function getPool() {
+  if (global.poolConnection) {
+    return global.poolConnection;
+  }
+
+  const poolConnection = mysql.createPool({
+    uri: config.DB_URL,
+  });
+
+  global.poolConnection = poolConnection;
+
+  return global.poolConnection;
+}
+
+const poolConnection = getPool();
 
 export const db = drizzle(poolConnection, { schema, mode: "default" });
